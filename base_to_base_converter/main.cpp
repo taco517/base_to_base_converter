4cfdf1d4ecb7f4f656d684617d0e1ae79f85fbed
tacortiz <thomasanthonyortiz@gmail.com>
2024-01-15 20:00:05 +0800

    fixed running issue

--- base_to_base_converter/main.cpp
+++ base_to_base_converter/main.cpp
@@ -1,14 +1,13 @@
-// EE 176 Exercise 0 by Thomas Anthony Ortiz
-// the coder tried to incorporate conio.h to the system but it didn't work since student used  WSL
-// ncurses.h was tried to call getch() but it didn't work
+// base to base converted by taco
+// inputs a number from base 2 to 16 then converts it to a chosen base
 
-#include <iostream> // Used for user prompted i/o
-#include <cmath> // Used specifically for the power exponent for converting the input into base-10
+#include <iostream>
+#include <cmath>
 #include <string>
 
 using namespace std;
 
-// Function to check if an input value contains valid digits or is in the proper format for the given base
+// function to check if an input value contains valid digits or is in the proper format for the given base
 bool are_digits_valid(const string & value, int base) {
     for (char digit : value) {
         digit = toupper(digit);
@@ -25,7 +24,7 @@ bool are_digits_valid(const string & value, int base) {
     return true;
 }
 
-// Function to check if the characters in a string is all numeric
+// function to check if the characters in a string is all numeric
 bool are_characters_numeric(const string & str) {
     for (char c : str) {
         if (!isdigit(c)) {
@@ -35,7 +34,7 @@ bool are_characters_numeric(const string & str) {
     return false; 
 }
 
-// Function to convert a number in base 2 to 16 to decimal
+// function to convert a number in base 2 to 16 to decimal
 int input_value_to_decimal(const string & number, int base) {
     int output_decimal = 0;
     int power = 0;
@@ -55,7 +54,7 @@ int input_value_to_decimal(const string & number, int base) {
     return output_decimal;
 }
 
-// Function to convert a number in decimal to any base
+// function to convert a number in decimal to any base
 string decimal_to_base_x(int input_decimal, int base) {
     string output_base_x = "";
     while (input_decimal > 0) {
@@ -77,7 +76,7 @@ int main() {
 
         cout << "\nEE 176\nExercise 0: Base to Base Converter\nby Thomas Anthony Ortiz\nEnter 'x' to exit.\n\n";
 
-        // Input the input base with validation such that the input base should only be within integers 2 to 16 
+        // input the input base with validation such that the input base should only be within integers 2 to 16 
         do {
             cout << "Enter the input base (2-16): ";
             cin >> input_base_str;
@@ -98,7 +97,7 @@ int main() {
                 continue;
             }
 
-            break; // If the input is a valid integer, break out of the loop
+            break; // ff the input is a valid integer, break out of the loop
             } catch (const invalid_argument& e) {
                 cin.clear();
                 cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
@@ -108,7 +107,7 @@ int main() {
 
         } while (true);
 
-        // Input the input value with validation such that the value should follow the proper format of the chosen base, 
+        // input the input value with validation such that the value should follow the proper format of the chosen base, 
         // and that the input value should not exceed the input equivalnet 255 base 10
         do {
             cout << "Enter input value in base-" << input_base << ": ";
@@ -136,7 +135,7 @@ int main() {
             break;
         } while (true);
 
-        // Input the output base with validation such that the output base should only be within integers 2 to 16
+        // input the output base with validation such that the output base should only be within integers 2 to 16
         do {
             cout << "Enter the output base (2-16): ";
             cin >> output_base_str;
@@ -157,7 +156,7 @@ int main() {
                 continue;
             }
 
-            break; // If the input is a valid integer, break out of the loop
+            break; // if the input is a valid integer, break out of the loop
             } catch (const invalid_argument& e) {
                 cin.clear();
                 cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
@@ -166,24 +165,24 @@ int main() {
 
         } while (true);
 
-        // Convert the number to decimal and binary using the input_value_to_decimal & decimal_to_binary functions
+        // convert the number to decimal and binary using the input_value_to_decimal & decimal_to_binary functions
         int output_base_decimal = input_value_to_decimal(input_value, input_base);
         string output_base_binary = decimal_to_base_x(output_base_decimal, 2);
 
 
-        // Convert the decimal result to the new base and display it only if the output base is not in base-2 or base-10
-        // This is to reduce redundancy since it is a requirement that the binary and decimal equivalents are always posted
+        // convert the decimal result to the new base and display it only if the output base is not in base-2 or base-10
+        // this is to reduce redundancy since it is a requirement that the binary and decimal equivalents are always posted
         if (!(output_base == 2 || output_base == 10)) {
         string output_base_result = decimal_to_base_x(output_base_decimal, output_base);
         cout << "\nBase-" << output_base << " equivalent: " << output_base_result << endl;
         }
 
-        // Display decimal and binary equivalent
+        // display decimal and binary equivalent
         cout << "\nDecimal equivalent: " << output_base_decimal << "\n";
         cout << "Binary equivalent: " << output_base_binary << "\n";
 
 
-        // Once the operation is done, code will prompt the user if they want to convert anotehr value
+        // once the operation is done, code will prompt the user if they want to convert another value
         do {
             char choice;
             cout << "\nConvert another? (y/n): ";
